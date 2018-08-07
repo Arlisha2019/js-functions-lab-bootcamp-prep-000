142b01cf6ed682386ed3e076e43023553bf94b38
Arlisha2019 <arlisha_hayles@yahoo.com>
2018-08-07 17:55:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,17 +2,6 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
-function happyHolidaysTo(Tiffany) {
-  return `Happy holidays, ${name}!`
-}
-
-function happyHolidaysTo(Easter, Tiffany) {
-  return `Happy ${hoilday}, ${name}!`
-}
-
-function hoildayCountdown(257, Easter) {
-  return `It's ${days} days until ${hoilday}!`
-}
 
 
 
